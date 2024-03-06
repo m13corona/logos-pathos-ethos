9ca659e1d899a94b7b40da9a57da5071d427ce88
m13corona <mcorona1308@gmail.com>
2024-03-06 17:12:36 -0600

    add code for user questions

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const fs = require ('fs');
 const inquirer = require('inquirer');
 
-const { generateLogo } = require 
+const { generateLogo } = require ('./Lib')
+
+function userQuestions() {
+    return inquirer.prompt([
+
+{
+    type: 'input',
+    name: 'text',
+    message: 'Please enter three characters maximum',
+},
+
+{
+
+    type: 'input',
+    name: 'color',
+    message: 'Please enter text color',
+
+},
+
+{
+    type: 'list',
+    name: 'shape',
+    message: 'Please choose between: circle, triangle, and square',
+    choices: ['cicle', 'triangle', 'square']
+
+
+
+}
+
+
+
+    ])
+
+
+}
